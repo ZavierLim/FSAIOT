17cac963986c64f5c8f4b39d50d3d90c942d5a1a
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-10-27 16:36:50 +0800

    Add sql file

---
+++ sql/sql script.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS `fsa`.`contractors`(
+	`ContractorId` INT NOT NULL AUTO_INCREMENT,
+    `Name` varchar(45) NOT NULL,
+    `Company` varchar(100) DEFAULT NULL,
+    `PhoneNumber` INT NOT NULL,
+    `Photo` varchar(255) DEFAULT NULL,
+    PRIMARY KEY(`ContractorId`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `fsa`.`Requests`(
+	`RequestId` INT NOT NULL AUTO_INCREMENT,
+    `ContractorId` INT,
+	`Timing` DATETIME,
+    `Approval` BOOLEAN,
+    PRIMARY KEY(`RequestId`),
+    FOREIGN KEY (`ContractorId`) 
+		REFERENCES contractors(ContractorId)
+    ON UPDATE CASCADE
+) ;
+
+CREATE TABLE IF NOT EXISTS `fsa`.`AccessHistory`(
+	`AccessId` INT NOT NULL AUTO_INCREMENT,
+    `RequestId` INT,
+    `DateTimeEntered` DATETIME,
+    PRIMARY KEY(`AccessId`),
+    FOREIGN KEY(`RequestId`)
+		REFERENCES requests(RequestId)
+    ON UPDATE CASCADE
+)
