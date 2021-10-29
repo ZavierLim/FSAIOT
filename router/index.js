babb89d15be92f5851f03e57ba997c8dec7eb35e
Hexuesheng-666 <e0696879@u.nus.edu>
2021-10-29 17:04:13 +0800

    clicksend

--- router/index.js
+++ router/index.js
@@ -9,5 +9,6 @@ router.get("/", async (req, res) => {
 router.use("/users", require("./apis/users"));
 router.use("/request", require("./apis/request"));
 router.use("/contractors", require("./apis/contractors"));
+router.use("/clicksend", require("./apis/clicksend"));
 
 module.exports = router;
