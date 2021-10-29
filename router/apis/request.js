cdd837f490b3dc95ad54e39e45513ed0a8a431c5
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-10-29 17:14:43 +0800

    routing from RAspberry to backend

--- router/apis/request.js
+++ router/apis/request.js
@@ -1,6 +1,31 @@
 const router = require("express").Router();
 const database = require("../../database");
+const Contractor = require("../../model/contractor");
 
-router.post("/requestaccess", (req, res) => res.send("login"));
+router.post("/requestaccess", async (req, res) => {
+  if (res.error) {
+    res.status(400).send(res.error.details[0].message);
+    return;
+  }
 
+  const contractor = new Contractor();
+  contractor.name = req.body.name;
+  contractor.company = req.body.company;
+  contractor.phonenumber = req.body.phonenumber;
+  contractor.photo = req.body.photo;
+
+  //  let sql = `SELECT * FROM contractors c WHERE c.name="${contractor.name}"`;
+  let sql = `SELECT * FROM contractors c WHERE c.name="${contractor.name}"`;
+  //destructure row packet data and resultsethea.der
+  const [tolog, _] = await database.execute(sql);
+  if (tolog[0] == undefined) {
+    res.send(null);
+    return;
+  }
+  res.send(tolog); //return the course;
+  console.log("ok");
+  return;
+});
+
+router.get("/requestaccess", (req, res) => res.send("login"));
 module.exports = router;
